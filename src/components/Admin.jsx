ef7e0ef25d5a26d0df7f29c938b065085d59d745
pankajdkoli <pankajdkoli@gmail.com>
2023-05-18 22:23:21 +0530

    make a changes

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./admin.css";
 
 const Admin = () => {
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,6 +5,17 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer-text">© 2023 Your Website. All rights reserved.</p>
+      <p>
+        Created with <i className="fa fa-heart"></i> by
+        <a target="_blank" href="https://www.linkedin.com/in/pankajdkoli">
+          Pankaj Koli
+        </a>
+        - Read how I created this and how you can join the challenge
+        <a target="_blank" href="https://github.com/pankajdkoli">
+          here
+        </a>
+        .
+      </p>
     </footer>
   );
 }
