fa2b1984dd6741c1e72a59956c9ecbc67724169a
pankajdkoli <pankajdkoli@gmail.com>
2023-05-19 14:43:26 +0530

    get userlist for backend

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function UserList() {
-  return <div>UserList</div>;
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/auth/getuser");
+        setUsers(response.data);
+      } catch (error) {
+        console.log("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>Email: {user.email}</p>
+          <p>Username: {user.username}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default UserList;
